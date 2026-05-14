03d2ada3234ac3d4dd4619612ab6d2cd47523c37
Shrikrishna <123809369+Krishna7420@users.noreply.github.com>
2026-05-14 15:21:50 +0530

    Initial Commit

---
+++ Rock_Paper_Scissors/Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorsApp.swift
+//  Rock_Paper_Scissors
+//
+//  Created by Shrikrishna Thodsare on 14/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
